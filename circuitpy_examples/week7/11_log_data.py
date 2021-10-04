0227ace4d27361dfa85c83ed3253ffffa5393c9c
Adam Beardsley <adam.p.beardsley@gmail.com>
2021-10-04 14:31:17 -0500

    clean up log data script

--- circuitpy_examples/week7/11_log_data.py
+++ circuitpy_examples/week7/11_log_data.py
@@ -10,43 +10,49 @@
 import time
 from adafruit_circuitplayground.express import cpx
 
-def get_accel_on_button():
-    x,y,z=cpx.acceleration
+g = 9.801
+log_time = 0.02  # Time to wait between logging points
+wait_time = 0.1  # Time to wait when listening for button
+
+def log_accel(file=None):
+    x, y, z = cpx.acceleration
     current_time = time.monotonic()
-    g=9.801
-    if cpx.button_a:
-        cpx.red_led = True
-        time.sleep(0.02)
-        fp.write('{0:f},{1:f},{2:f},{3:f}\n'.format(current_time,x/g,y/g,z/g))
-        print('logging',(current_time,x/g,y/g,z/g))
-        fp.flush()
-    else:
-        cpx.red_led = False
-        time.sleep(0.1)
-        print((current_time,x/g,y/g,z/g))
+    outstr = f'{current_time},{x / g},{y / g},{z / g}\n'
+    if file is not None:
+        file.write(outstr)
+        outstr = 'logging: ' + outstr
+    print(outstr)
     return
 
-#will try and see if a file is writeable, if it is it continues, if not it goes to the except statement
+def get_accel_on_button(file):
+    cpx.red_led = False
+    while not cpx.button_a:
+        log_accel()
+        time.sleep(wait_time)
+
+    cpx.red_led = True
+    while cpx.button_a:
+        log_accel(file)
+        time.sleep(log_time)
+    file.flush()
+
+    return
+
+# See if a file is writeable, if it is it continues, if not it goes to the except statement
 try:
-    #open a file to save the data to
-    with open("/acceleration.txt", "a") as fp:
-        while True:
-            get_accel_on_button()
+    while True:
+        # open a file to save the data to
+        with open("/acceleration.csv", "a") as fp:
+            get_accel_on_button(fp)
 
 # will flash the D13 red LED to let us know we can edit the code.py file (i.e. the storage is writeable)
 except OSError as e:
-    delay = 8
+    delay = 1.0
     if e.args[0] == 28:
-        delay = 4
+        # If file system is full, blink faster
+        delay = 0.2
     while True:
-        i = 0
-
-        while (i<delay):
-            get_accel_on_button()
-            cpx.red_led = True
-            i=i+1
-
-        while (i>0):
-            get_accel_on_button()
-            cpx.red_led = False
-            i=i-1
+        cpx.red_led = True
+        time.sleep(delay)
+        cpx.red_led = False
+        time.sleep(delay)
