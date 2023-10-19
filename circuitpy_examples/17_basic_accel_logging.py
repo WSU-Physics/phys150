da61e0e3570859796bc15eeb2e7091c814360ca6
Nathan Moore <ntmoore@gmail.com>
2023-10-19 17:21:44 -0500

    Update 17_basic_accel_logging.py

--- circuitpy_examples/17_basic_accel_logging.py
+++ circuitpy_examples/17_basic_accel_logging.py
@@ -1,12 +1,7 @@
 from adafruit_circuitplayground import cp
 import time
 
-# wait time in s
-wait_time = 0.1
-
 while True:
     x, y, z = cp.acceleration
-    t = time.monotonic() #https://docs.circuitpython.org/en/latest/shared-bindings/time/
-    print(t,",",x,",",y,",",z,",") # this line for csv plotting
-    #     print((x,y,z))    # this line for interactive plotting
+    print((x,y,z))    # this line for interactive plotting
     time.sleep(wait_time)
