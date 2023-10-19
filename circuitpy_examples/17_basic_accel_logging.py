091e12ca1f1a51ef0bb4ceccefec1ebc0df58c1e
Nathan Moore <ntmoore@gmail.com>
2023-10-19 15:53:51 -0500

    Update 17_basic_accel_logging.py

--- circuitpy_examples/17_basic_accel_logging.py
+++ circuitpy_examples/17_basic_accel_logging.py
@@ -7,5 +7,6 @@ wait_time = 0.1
 while True:
     x, y, z = cp.acceleration
     t = time.monotonic() #https://docs.circuitpython.org/en/latest/shared-bindings/time/
-    print(t,",",x,",",y,",",z,",")
+    print(t,",",x,",",y,",",z,",") # this line for csv plotting
+    #     print((x,y,z))    # this line for interactive plotting
     time.sleep(wait_time)
