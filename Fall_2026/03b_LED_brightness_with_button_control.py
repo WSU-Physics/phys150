f27b9b0e37becfab9558334ff46a1e3123f2037d
Andrew Ferstl <111376279+andyferstl@users.noreply.github.com>
2026-09-03 14:51:04 -0500

    Update 03b_LED_brightness_with_button_control.py

--- Fall_2026/03b_LED_brightness_with_button_control.py
+++ Fall_2026/03b_LED_brightness_with_button_control.py
@@ -9,6 +9,12 @@ import time
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
+button_a = digitalio.DigitalInOut(board.BUTTON_A)
+button_a.switch_to_input(pull=digitalio.Pull.DOWN)
+
+button_b = digitalio.DigitalInOut(board.BUTTON_B)
+button_b.switch_to_input(pull=digitalio.Pull.DOWN)
+
 # Choose a brightness between 0 and 1
 brightness = .01
 # Time per cycle, in seconds
@@ -24,9 +30,9 @@ while True:
     time.sleep(T_off)
     # How can we use the buttons on the CPX to change the brightness while the program is running?
     # looking at the documentation, https://learn.adafruit.com/circuit-playground-lesson-number-0/buttons-slide-switch
-    if(CircuitPlayground.leftButton()):
+    if(button_a):
         period = period*0.9
-    if(CircuitPlayground.rightButton()):
+    if(button_b):
         period = period*1.1
     print(period)
         
